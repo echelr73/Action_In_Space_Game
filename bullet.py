2275f8419e6d7d87213851be7389cab00db72b27
Martin Echeverria <martin.echeverria@accenture.com>
2024-02-25 13:30:28 -0300

    Add comments

--- bullet.py
+++ bullet.py
@@ -1,9 +1,11 @@
 import pygame
 import random
 
+# Screen dimensions
 weight = 800
 height = 600
 
+# Colors
 black = (0, 0, 0)
 white = (255, 255, 255) 
 red = (255, 0, 0)   
@@ -15,14 +17,18 @@ H_FA2F2F = (250, 47, 47)
 class bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
+        # Load and scale bullet image
         self.image = pygame.image.load("bullet.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (5, 15))
+        # Uncomment line below to draw a red rectangle around the bullet for visualization
         """pygame.draw.rect(self.image, red, self.image.get_rect(), 1)"""
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
     def update(self):
+        # Move bullet upwards
         speed = 10
         self.rect.y -= speed
+        # Remove bullet if it goes off the screen
         if self.rect.top < 0:
             self.kill()
--- enemy.py
+++ enemy.py
@@ -1,9 +1,11 @@
 import pygame
 import random
 
+# Screen dimensions
 weight = 800
 height = 600
 
+# Colors
 black = (0, 0, 0)
 white = (255, 255, 255) 
 red = (255, 0, 0)   
@@ -12,24 +14,29 @@ blue = (0, 0, 255)
 yellow = (255, 255, 0)
 H_FA2F2F = (250, 47, 47)
 
-
 class enemy(pygame.sprite.Sprite):
-    def __init__(self,speed):
+    def __init__(self, speed):
         super().__init__()
+        # Load and scale enemy image
         self.image = pygame.image.load("enemy.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (50, 25))
+        # Uncomment line below to draw a red rectangle around the enemy for visualization
         """pygame.draw.rect(self.image, red, self.image.get_rect(), 1)"""
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(0,weight - self.rect.width),
-                                         random.randint(0, 100 - self.rect.height))
+        # Randomize initial position within screen boundaries
+        self.rect.center = (random.randint(0, weight - self.rect.width),
+                            random.randint(0, 100 - self.rect.height))
 
+        # Set random velocities within specified speed limit
         self.velocity_x = random.randint(1, speed)
         self.velocity_y = random.randint(1, speed)
 
     def update(self):
+        # Move the enemy horizontally and vertically
         self.rect.x -= self.velocity_x
         self.rect.y += self.velocity_y
 
+        # Adjust velocities if enemy hits screen boundaries
         if self.rect.left < 0:
             self.velocity_x -= 1
 
@@ -41,5 +48,3 @@ class enemy(pygame.sprite.Sprite):
         
         if self.rect.right > weight:
             self.velocity_x += 1
-        
-               
